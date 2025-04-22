f8fe820bc1a246ac9d94a8d1254cb014a52cd775
priyank2809 <priyank2806@gmail.com>
2025-04-22 18:34:13 +0530

    path changes

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -29,11 +29,11 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 11671, hash: 'c7376da5aca6d1d58cfc3f66e85b6bd732763f4d8fad43efebef0bdadb6bac74', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1209, hash: '943d18028b08135587076f70c37f3e22eef83b6cde2f7b67e3817e6c045f4c35', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 11671, hash: '9d232f39f0f053ea2a7f3a4e88f0b597889ff137d3d40e7db77231790da9ea94', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1209, hash: '97d343068721bbf1d2b1f96370af9fc4087f6e71b700bb4019e7a27d792a469b', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'svg/index.html': {size: 23923, hash: 'c493ed9d8af366439aa7b272ec348e1b1a4237456fba49c26d416b32b381a8f0', text: () => import('./assets-chunks/svg_index_html.mjs').then(m => m.default)},
-    'cards/index.html': {size: 25715, hash: 'e4faa5875e43da6360a56edd4d6e72d54156efc7c183ffb947d6f5cfef87fd13', text: () => import('./assets-chunks/cards_index_html.mjs').then(m => m.default)},
     'text/index.html': {size: 24297, hash: '4f931601c5679c522cf662a66cffd05c0b37326b01fe3fd08bef2f1c156213ee', text: () => import('./assets-chunks/text_index_html.mjs').then(m => m.default)},
+    'cards/index.html': {size: 25715, hash: '07a4f54ac62270eca2e00bf229697c2537b5d722a227ea07f3ec573e2fe35b6e', text: () => import('./assets-chunks/cards_index_html.mjs').then(m => m.default)},
     'timeline/index.html': {size: 27156, hash: '27e7f7ae9afde0f2ae82793729773a4e10ea1d8e4796588b5fe03e6041e3234f', text: () => import('./assets-chunks/timeline_index_html.mjs').then(m => m.default)},
     'styles-H7IIGEV5.css': {size: 11050, hash: 'jkWUbeF1zNw', text: () => import('./assets-chunks/styles-H7IIGEV5_css.mjs').then(m => m.default)}
   },
