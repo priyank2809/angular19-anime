5fe2d0c0df7f09835e5e5e7c1457310cfd69e676
priyank2809 <priyank2806@gmail.com>
2025-05-01 21:45:25 +0530

    Auto-generated commit

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -31,10 +31,10 @@ export default {
   assets: {
     'index.csr.html': {size: 11671, hash: '9d232f39f0f053ea2a7f3a4e88f0b597889ff137d3d40e7db77231790da9ea94', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
     'index.server.html': {size: 1209, hash: '97d343068721bbf1d2b1f96370af9fc4087f6e71b700bb4019e7a27d792a469b', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'svg/index.html': {size: 23923, hash: 'c493ed9d8af366439aa7b272ec348e1b1a4237456fba49c26d416b32b381a8f0', text: () => import('./assets-chunks/svg_index_html.mjs').then(m => m.default)},
     'text/index.html': {size: 24297, hash: '4f931601c5679c522cf662a66cffd05c0b37326b01fe3fd08bef2f1c156213ee', text: () => import('./assets-chunks/text_index_html.mjs').then(m => m.default)},
-    'cards/index.html': {size: 25715, hash: 'ba4a74fe354391cad1054c72d956c6c66ebbef8515fca1f79ee0020752f70f1a', text: () => import('./assets-chunks/cards_index_html.mjs').then(m => m.default)},
+    'svg/index.html': {size: 23923, hash: 'c493ed9d8af366439aa7b272ec348e1b1a4237456fba49c26d416b32b381a8f0', text: () => import('./assets-chunks/svg_index_html.mjs').then(m => m.default)},
     'timeline/index.html': {size: 27156, hash: '27e7f7ae9afde0f2ae82793729773a4e10ea1d8e4796588b5fe03e6041e3234f', text: () => import('./assets-chunks/timeline_index_html.mjs').then(m => m.default)},
+    'cards/index.html': {size: 25715, hash: '72b2396058e9b4d114ef5b4227340e41caa5dde69ec3e1373310105fb511e0a7', text: () => import('./assets-chunks/cards_index_html.mjs').then(m => m.default)},
     'styles-H7IIGEV5.css': {size: 11050, hash: 'jkWUbeF1zNw', text: () => import('./assets-chunks/styles-H7IIGEV5_css.mjs').then(m => m.default)}
   },
 };
